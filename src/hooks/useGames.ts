0b50c0cfda4d9508dbb18e0c3395217dd8bf322a
im-krgourav <gouravkr83@gmail.com>
2023-09-26 23:44:23 +0530

    Refactoring: Fetch games, genre and platform with react query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
+import apiClient, { FetchResponse } from "../services/api-client";
 
 export interface Platform {
   id: number;
@@ -17,18 +18,22 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-      },
-    },
-    [gameQuery]
-  );
+  useQuery<FetchResponse<Game>, Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>("/games", {
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24hrs
+    // initialData: { count: geners.length, results: geners },
+  });
 
 // this is generically defined in useData hook. so doesn't need to be defined here
 // {
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient, { FetchResponse } from "../services/api-client";
 
-// interface Platform {
-//   id: number;
-//   name: string;
-//   slug: string;
-// }
+interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 
 // const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
-const usePlatforms = () => ({ data: platforms, error: null });
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24hrs
+    initialData: { count: platforms.length, results: platforms },
+  });
 export default usePlatforms;
